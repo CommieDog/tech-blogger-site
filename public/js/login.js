8b1566dfee8946720d541db33a7e900ed9d9679c
John Netzel <johnrnetzel@gmail.com>
2022-05-07 14:23:12 -0800

    Add API calls via fetch() to login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,47 @@
 const signInForm = document.querySelector("#sign-in-form");
 const signUpForm = document.querySelector("#sign-up-form");
+const signInEmailField = document.querySelector("#sign-in-email");
+const signInPasswordField = document.querySelector("#sign-in-password");
+const signUpUsernameField = document.querySelector("#sign-up-username");
+const signUpEmailField = document.querySelector("#sign-up-email");
+const signUpPasswordField = document.querySelector("#sign-up-password");
 
-function submitSignInForm(event)
+async function submitSignInForm(event)
 {
     event.preventDefault();
-    alert("submitSignInForm called!");
+    const response = await fetch("api/users/login/", {
+        method: 'POST',
+        body: JSON.stringify({ email: signInEmailField.value.trim(), password: signInPasswordField.value.trim() }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if(response.ok)
+    {
+        document.location.replace("/"); // Return to homepage
+    }
+    else
+    {
+        alert("Unable to sign in");
+    }
 }
 
-function submitSignUpForm(event)
+async function submitSignUpForm(event)
 {
     event.preventDefault();
-    alert("submitSignUpForm called!");
+    const response = await fetch("api/users/login/", {
+        method: 'POST',
+        body: JSON.stringify({ username: signUpUsernameField.value.trim(), email: signUpEmailField.value.trim(), password: signUpPasswordField.value.trim() }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if(response.ok)
+    {
+        document.location.replace("/"); // Return to homepage
+    }
+    else
+    {
+        alert("Unable to sign up");
+    }
 }
 
 signInForm.addEventListener("submit", submitSignInForm);
