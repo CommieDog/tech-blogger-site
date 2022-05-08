b8301aa95fe5167adfefe915edbb5fc82ffe2e20
John Netzel <johnrnetzel@gmail.com>
2022-05-07 19:32:39 -0800

    Add dashboard page and route

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 // const { User } = require('../models');
 // const withAuth = require('../utils/auth');
 
-// Prevent non logged in users from viewing the homepage
-router.get('/', /*withAuth,*/ async (req, res) => {
+router.get('/', async (req, res) => {
 //   try {
 //     const userData = await User.findAll({
 //       attributes: { exclude: ['password'] },
@@ -21,6 +20,27 @@ router.get('/', /*withAuth,*/ async (req, res) => {
 //     res.status(500).json(err);
 //   }
     res.render("front-page");
+});
+
+// Prevent non logged in users from viewing the homepage
+router.get('/dashboard', /*withAuth,*/ async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ['password'] },
+//       order: [['name', 'ASC']],
+//     });
+
+//     const users = userData.map((project) => project.get({ plain: true }));
+
+//     res.render('homepage', {
+//       users,
+//       // Pass the logged in flag to the template
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+    res.render("dashboard");
 });
 
 router.get('/login', (req, res) => {
