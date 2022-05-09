1d8a2222718d864b1472668592dd0569cc46d4a3
John Netzel <johnrnetzel@gmail.com>
2022-05-09 15:30:00 -0800

    Fix logout option not appearing on homepage when logged in

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
 
     res.render('front-page', {
       posts,
-      logged_in: req.session.logged_in,
+      loggedIn: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
